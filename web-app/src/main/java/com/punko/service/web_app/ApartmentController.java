a9ab9458e547c738cbb5fd2b38bebb5b397b06fa
Vitaliy Punko <punkovitaliy@gmail.com>
2021-03-31 17:03:59 +0300

    fixed links in html

--- web-app/src/main/java/com/punko/service/web_app/ApartmentController.java
+++ web-app/src/main/java/com/punko/service/web_app/ApartmentController.java
@@ -3,6 +3,7 @@ package com.punko.service.web_app;
 import com.punko.ApartmentDtoService;
 import com.punko.ApartmentService;
 import com.punko.dao.ApartmentDaoDto;
+import com.punko.model.Apartment;
 import com.punko.model.dto.ApartmentDto;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,30 +11,56 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/apartments")
 public class ApartmentController {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(ApartmentController.class);
-
-    private final ApartmentService apartmentService;
-
-    private final ApartmentDtoService apartmentDtoService;
-
     @Autowired
-    public ApartmentController(ApartmentService apartmentService, ApartmentDtoService apartmentDtoService) {
-        this.apartmentService = apartmentService;
-        this.apartmentDtoService = apartmentDtoService;
-    }
+    ApartmentService apartmentService;
 
-    @GetMapping(value = "/apartments")
-    public final String apartments(Model model) {
-        LOGGER.debug("apartments()");
-        List<ApartmentDto> apartmentDaoDtoList = apartmentDtoService.findAllWithAvgTime();
-        model.addAttribute("allApartments", apartmentDaoDtoList);
+    @RequestMapping("")
+    public String showAllApartment(Model model) {
+        List<Apartment> apartmentList = apartmentService.findAll();
+        model.addAttribute("allApartments", apartmentList);
+
         return "apartments";
     }
 
+    @RequestMapping("/addApartment")
+    public String addApartment(Model model) {
+        Apartment apartment = new Apartment();
+        model.addAttribute("addApartmentAttribute", apartment);
+        return "apartmentPage";
+    }
+
+    @RequestMapping("/saveApartment")
+    public String saveApartment(@ModelAttribute("addApartmentAttribute") Apartment apartment) {
+        apartmentService.create(apartment);
+        return "redirect:/apartments";
+    }
+
+    /**
+     *  name of attribute must be the same like in addApartment
+     *  because they return the same jsp page
+     */
+    @RequestMapping("/updateApartment/{id}")
+    public String updateApartment(@PathVariable int id, Model model) {
+        Apartment apartment = apartmentService.findById(id);
+        model.addAttribute("addApartmentAttribute", apartment);
+        return "apartmentPage";
+    }
+
+    @RequestMapping("/deleteApartment/{id}")
+    public String deleteApartment(@PathVariable int id) {
+        apartmentService.delete(id);
+        return "redirect:/apartments";
+    }
+
 }
+
